5be93aff3af16c387f5944677ff1728d2ad3d702
ZakariaNYA <zakaria.nya@cgi.com>
2019-10-01 09:57:54 +0200

    [Feat] Add the board with canvas api web

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { COLS, ROWS, BLOCK_SIZE } from '../constants';
+
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+
+
+export class BoardComponent implements OnInit {
+
+  @ViewChild('board', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+
+
+  ctx: CanvasRenderingContext2D;
+  points: number;
+  lines: number;
+  level: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  initBoard() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+
+
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+  }
+
+
+
+  play() { }
+
+}
