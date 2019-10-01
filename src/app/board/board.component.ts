72c909ebb1d3fd864134f15800ded6d3a65b0329
ZakariaNYA <zakaria.nya@cgi.com>
2019-10-01 10:22:19 +0200

    [FEAT] add the game's style

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -14,29 +14,23 @@ export class BoardComponent implements OnInit {
   @ViewChild('board', { static: true })
   canvas: ElementRef<HTMLCanvasElement>;
 
-
-
   ctx: CanvasRenderingContext2D;
   points: number;
   lines: number;
   level: number;
 
-  constructor() { }
 
   ngOnInit() {
+    this.initBoard();
   }
 
+  
 
   initBoard() {
     this.ctx = this.canvas.nativeElement.getContext('2d');
-
-
     this.ctx.canvas.width = COLS * BLOCK_SIZE;
     this.ctx.canvas.height = ROWS * BLOCK_SIZE;
-
   }
-
-
 
   play() { }
 
